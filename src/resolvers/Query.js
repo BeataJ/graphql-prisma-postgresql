e611f72f17ef6b9660a657533efd375512ea5796
beata <beata@emc2wave.com>
2020-11-24 15:12:33 -0800

    duplicate

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,12 +1,14 @@
 const Query = {
   users: (parent, args, { db, prisma }, info) => {
-    if (!args.query) {
-      return db.users;
-    }
+    prisma.query.users(null, info);
 
-    return db.users.filter((user) => {
-      return user.name.toLowerCase().includes(args.query.toLowerCase());
-    });
+    // if (!args.query) {
+    //   return db.users;
+    // }
+
+    // return db.users.filter((user) => {
+    //   return user.name.toLowerCase().includes(args.query.toLowerCase());
+    // });
   },
   posts: (parent, args, { db }, info) => {
     if (!args.query) {
