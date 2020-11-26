ce91f1517b58ad46218faed1098d28b2d55a9f0b
beata <beata@emc2wave.com>
2020-11-25 20:36:04 -0800

    delete code from query..

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -38,23 +38,6 @@ const Query = {
   comments: (parent, args, { prisma }, info) => {
     return prisma.query.comments(null, info);
   },
-  me: () => {
-    return {
-      id: '12345',
-      name: 'Mike',
-      email: 'mail@example.ca',
-    };
-  },
-  post: () => {
-    return {
-      id: '98765',
-      title:
-        'Missing couple rescued after frigid night spent in North Shore mountains',
-      body:
-        'Two overdue hikers have been found alive and well by search and rescue crews after spending a cold and wet night stranded in the North Shore. ',
-      published: true,
-    };
-  },
 };
 
 export { Query as default };
