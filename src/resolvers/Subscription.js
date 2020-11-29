f2b0b28fd2cfea341781d379cb0a672a663e3e9b
beata <beata@emc2wave.com>
2020-11-29 01:19:15 -0800

    subscribe comment with postId

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,18 +1,18 @@
 const Subscription = {
   comment: {
     subscribe: (parent, { postId }, { prisma }, info) => {
-      return prisma.subscription.comment(null, info);
-
-      //   const post = db.posts.find(
-      //     (post) => post.id === postId && post.published
-      //   );
-
-      //   if (!post) {
-      //     throw new Error('Post not found');
-      //   }
-
-      //   return pubsub.asyncIterator(`comment ${postId}`);
-      // },
+      return prisma.subscription.comment(
+        {
+          where: {
+            node: {
+              post: {
+                id: postId,
+              },
+            },
+          },
+        },
+        info
+      );
     },
   },
   post: {
