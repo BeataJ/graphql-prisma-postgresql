9d3363b4c70359f311ddafc876fb4e0158930541
beata <beata@emc2wave.com>
2020-12-01 21:20:14 -0800

    code getUserId

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,3 +1,16 @@
-const getUserId = () => {};
+import jwt from 'jsonwebtoken';
+
+const getUserId = (request) => {
+  const header = request.request.headers.authorization;
+
+  if (!header) {
+    throw new Error('Authentitation required!');
+  }
+
+  const token = header.replace('Bearer', '');
+  const decoded = jwt.verify(token, 'thisisasecret');
+
+  return decoded.userId;
+};
 
 export { getUserId as default };
