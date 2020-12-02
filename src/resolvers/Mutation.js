eb3df1186536df091dba6bbae6b898c9efacbd05
beata <beata@emc2wave.com>
2020-12-01 21:22:03 -0800

    change args.data.author to userId

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -76,7 +76,7 @@ const Mutation = {
           published: args.data.published,
           author: {
             connect: {
-              id: args.data.author,
+              id: userId,
             },
           },
         },
