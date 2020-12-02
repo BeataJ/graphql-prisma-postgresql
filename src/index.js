9f05665ed2d1c4d172a0776e8d4c1b44a6b97d93
beata <beata@emc2wave.com>
2020-12-01 20:38:17 -0800

    context as function

--- src/index.js
+++ src/index.js
@@ -20,10 +20,14 @@ const server = new GraphQLServer({
     Post,
     Comment,
   },
-  context: {
-    db,
-    pubsub,
-    prisma,
+  context: (request) => {
+    console.log(request);
+    return {
+      db,
+      pubsub,
+      prisma,
+      request,
+    };
   },
 });
 
