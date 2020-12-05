146dc99fabb26b9af737a392ab270c99541f81fc
beata <beata@emc2wave.com>
2020-12-04 19:36:10 -0800

    create const User

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,3 +1,11 @@
-const User = {};
+import getUserId from '../utils/getUserId';
+
+const User = {
+  email: (parent, args, { request }, info) => {
+    const userId = getUserId(request, false);
+
+    console.log(parent);
+  },
+};
 
 export { User as default };
