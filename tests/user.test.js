38dcc7aa61363e7e967b327b449704d1cce4587c
beata <beata@emc2wave.com>
2021-01-25 18:39:51 -0800

    add expect.toBe

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,7 +3,9 @@ const getFirstName = require('../src/utils/user');
 test('Should return first name when given full name', () => {
   const firstName = getFirstName('Beata Jasinska');
 
-  if (firstName !== 'Beata') {
-    throw new Error('Expected the string  Beata');
-  }
+  expect(firstName).toBe('Beata');
+
+  // if (firstName !== 'Beata') {
+  //   throw new Error('Expected the string  Beata');
+  // }
 });
